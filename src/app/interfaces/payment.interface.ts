9dcaf0b5143d46999c7ed92c649786ca26340b92
cristiandev <cristiansotomayor.dev@gmail.com>
2021-11-10 23:55:16 -0500

    creating a class to implement the logic of the interface

--- src/app/interfaces/payment.interface.ts
+++ src/app/interfaces/payment.interface.ts
@@ -1,12 +1,48 @@
+import { paymentMethods } from "../mock/payment.mock";
+
 export interface IPayment {
   id?: number;
   name?: string;
   amount: number;
-  type: number;
+  method: number;
 }
 
-export interface IPaymentType {
+export interface IPaymentMethod {
   name: string;
   value: number;
   icon: string;
+}
+
+export class Payment implements IPayment {
+  id?: number;
+  name?: string;
+  amount: number;
+  method: number;
+
+  constructor(payment: IPayment) {
+    this.id = payment.id;
+    this.name = payment.name;
+    this.amount = payment.amount;
+    this.method = payment.method;
+  }
+
+  mock() {
+    const random = this.random();
+
+    const randomMethod = paymentMethods[
+      Math.floor(Math.random() * paymentMethods.length)
+    ];
+
+    return {
+      id: random,
+      name: 'Payment' + random,
+      amount: 100,
+      method: randomMethod,
+    };
+  }
+
+  random() {
+    let num = Math.random() * (50000) + 1;
+    return Math.floor(num);
+  }
 }
